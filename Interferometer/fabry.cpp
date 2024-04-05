5765771cbb515a930b6cdf151968186ef974b44f
Emma <165904889+EmmaGiussani@users.noreply.github.com>
2024-04-05 09:20:23 +0200

    Add files via upload

---
+++ Interferometer/fabry.cpp
@@ -0,0 +1,52 @@
+/*
+c++ -o fabry fabry.cpp `root-config --cflags --glibs`
+*/
+
+#include <iostream>
+
+#include <TStyle.h>
+#include <TApplication.h>
+#include <TCanvas.h>
+#include <TF1.h>
+#include <TGraphErrors.h>
+#include <TAxis.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+  if (argc < 2)
+    {
+      cout << "Not enough parameters: ./fitretta filedati.txt" << endl;
+      return 1;
+    }
+  
+  gStyle->SetOptFit(1112);
+
+  TApplication* myApp = new TApplication("myApp", NULL, NULL);
+  TCanvas* myC = new TCanvas("myC","myC",0,0,700,500);
+  TGraphErrors* myGraph = new TGraphErrors(argv[1]);
+  myGraph->SetMarkerStyle(20);
+  myGraph->SetTitle("Calibrazione micrometro: #Deltad' vs. #DeltaN");
+  myGraph->GetXaxis()->SetTitle("#Delta N");
+  myGraph->GetYaxis()->SetTitle("#Delta d' [#mum]");
+  
+  TF1* myFun = new TF1 ("myFun","[0]+[1]*x");
+  myFun->SetParName(0,"A");
+  myFun->SetParName(1,"B");
+  myFun->SetParameter(0,-4);
+  myFun->SetParameter(1,1);
+
+  myC->cd();
+  myGraph->Draw("AP");
+  myGraph->Fit("myFun");   
+  myC->Modified();
+  myC->Update();
+
+  cout << "\nThe result of the fit with the model y(x) = A+Bx is:" << endl;
+  cout << "A = " << myFun->GetParameter(0) << " +/- " << myFun->GetParError(0) << endl;
+  cout << "B = " << myFun->GetParameter(1) << " +/- " << myFun->GetParError(1) << endl;
+    
+  myApp->Run();
+  return 0;
+}
